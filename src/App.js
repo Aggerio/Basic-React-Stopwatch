b6b8605a5fd247b43a39ec847837e2776ad8fcba
Aggerio <70311606+Aggerio@users.noreply.github.com>
2023-03-10 18:48:33 +0530

    Added source code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Stopwatch from "./Components/Stopwatch/Stopwatch.jsx";
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1 > React Stopwatch </h1>
+            <Stopwatch />
+        </div>
+    );
+}
+
+export default App;
